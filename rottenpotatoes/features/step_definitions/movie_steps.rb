5b0add6f3eaaeeb6ee088f212a7b9426869390f8
Reed Hampton <reedhampton@tamu.edu>
2019-03-27 02:29:11 +0000

    Finished edits to teh cucumber steps for PA4. Minor edits made to the step files along with references to these functions in teh feature files

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -4,8 +4,8 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create!(movie)
   end
-  fail "Unimplemented"
 end
 
 Then /(.*) seed movies should exist/ do | n_seeds |
@@ -18,7 +18,7 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+  page.body.index(e1).to_i < page.body.index(e2).to_i
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -29,10 +29,20 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  rating_list.split(', ').each do | r |
+    rating_text = "ratings_#{r}"
+    if uncheck
+      uncheck(rating_text)
+    else
+      check(rating_text)
+    end
+  end
 end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+  
+  #add one to account for the header row
+  number_of_movies  = Movie.count().to_i + 1
+  page.should have_css("table#movies tr", :count=>number_of_movies)
 end
